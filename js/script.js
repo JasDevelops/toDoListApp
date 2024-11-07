74ce4366b276779a2c40fd75fa10510a70b2061e
Jas <50244053+JasDevelops@users.noreply.github.com>
2024-11-07 19:30:55 +0100

    Add cross through function
    * add function and change position in function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-$(function() { 
-    function newItem() { 
-    let list = $('#list'); // assign list to variable
-    let inputValue = $('#input').val(); // store value of input field
-    let li = $('<li></li>'); // create new <li>
+$(function () {
+    function newItem() {
+        // add new item
+        let list = $('#list'); // assign list to variable
+        let inputValue = $('#input').val(); // store value of input field
+        let li = $('<li></li>'); // create new <li>
 
-    li.append(inputValue); //append inputValue
-    list.append(li); //append <li> from list
-}
+        li.append(inputValue); //append inputValue
+        list.append(li); //append <li> from list
 
-// Link function to click-button
-$('#button').click(newItem);
+        // add crossing out function
+        li.on('dblclick', function () {
+            li.addClass('strike'); // cross out 
+        });
+    }
+    // Link function to click-button
+    $('#button').click(newItem);
 });
