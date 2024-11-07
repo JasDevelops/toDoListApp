55410ceff41ad9d7372ac860cc3898cd75417b86
Jas <50244053+JasDevelops@users.noreply.github.com>
2024-11-07 19:19:28 +0100

    Link function to add-button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-function addItem() {
+$(function() { 
+    function newItem() { 
     let list = $('#list'); // assign list to variable
     let inputValue = $('#input').val(); // store value of input field
     let li = $('<li></li>'); // create new <li>
-    
+
     li.append(inputValue); //append inputValue
     list.append(li); //append <li> from list
 }
+
+// Link function to click-button
+$('#button').click(newItem);
+});
